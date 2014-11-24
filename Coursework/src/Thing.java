385323b8372db5e79d8b69f6033b97cbfc9075ac
dmd1337 <dmd1337@hotmail.co.uk>
2014-11-24 16:47:52 +0000

    I DID A THING

--- Coursework/src/Thing.java
+++ Coursework/src/Thing.java
@@ -1,14 +1,11 @@
-
-public class Thing {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Thing t = new Thing();
+public class Thing
+{
+	public static void main(String[] args)
+	{
+		Thing t = new Thing();
 	}
-	
 	public Thing()
 	{
 		System.out.println("helloThereThisIsATest");
 	}
-
 }
