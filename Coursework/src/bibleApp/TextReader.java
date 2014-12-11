a8785429bc9bf99c06d9a0fcf03ebee46174606e
dmd1337 <dmd1337@hotmail.co.uk>
2014-12-11 12:21:11 +0000

    .

--- Coursework/src/bibleApp/TextReader.java
+++ Coursework/src/bibleApp/TextReader.java
@@ -4,16 +4,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 /**
- * A class to read text from a specified file.
+ * An abstract class to read text from a specified book.
  * @author Norfolk-in-Chance (Thomas Slate, Jack Taylor, Abigail Boyton)
  */
-public class TextReader
+public abstract class TextReader
 {
-	private Scanner s;
-	/**
-	 * Constructor
-	 * @param file The file path for the scanner.
-	 */
+	protected Scanner s;
 	public TextReader(String file)
 	{
 		try
@@ -23,49 +19,10 @@ public class TextReader
 		catch (FileNotFoundException e)
         {
             System.err.println(e.getMessage());
-            System.exit(1);
         }
 	}
 	/**
-	 * @return A regular book's title
+	 * @return The book's title
 	 */
-	public String readTitleC()
-	{
-		s.reset();
-		s.useDelimiter("\nCHAPTER");
-		return s.next();
-	}
-	/**
-	 * @return The book of Psalms' title
-	 */
-	public String readTitleP()
-	{
-		s.reset();
-		s.useDelimiter("\nPSALM");
-		return s.next();
-	}
-	/**
-	 * @return The book's chapters.
-	 */
-	public String readChapters()
-	{
-		s.reset();
-		s.useDelimiter("\nCHAPTER");
-		s.next();
-		String text = "";
-		while (s.hasNext()) text += "CHAPTER" + s.next();
-		return text;
-	}
-	/**
-	 * @return The book's psalms.
-	 */
-	public String readPsalms()
-	{
-		s.reset();
-		s.useDelimiter("\nPSALM");
-		s.next();
-		String text = "";
-		while (s.hasNext()) text += "PSALM" + s.next();
-		return text;
-	}
+	public abstract String readTitle();
 }
