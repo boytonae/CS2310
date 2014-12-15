fa32aa9baf05434bd49b7a9a33d3379f11a7ccbb
dmd1337 <dmd1337@hotmail.co.uk>
2014-12-15 19:53:23 +0000

    Other stuff

--- Coursework/src/exceptions/YouEnteredTheWrongThingYouShouldBeVeryAshamedOfYourselfWhyWouldYouEvenDoThatException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-public class YouEnteredTheWrongThingYouShouldBeVeryAshamedOfYourselfWhyWouldYouEvenDoThatException extends Exception
-{
-	private static final long serialVersionUID = 6584085561057237924L;
-	public YouEnteredTheWrongThingYouShouldBeVeryAshamedOfYourselfWhyWouldYouEvenDoThatException()
-	{
-		super("You entered the wrong thing. You should be very ashamed of yourself. WHY WOULD YOU EVEN DO THAT?");
-	}
-}
