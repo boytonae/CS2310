49c2cdb2ca64f63e25404c332762c034f0901301
dmd1337 <dmd1337@hotmail.co.uk>
2014-12-16 19:59:36 +0000

    Removed UnknownErrorException

--- Coursework/src/exceptions/UnknownErrorException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-public class UnknownErrorException extends Exception
-{
-	private static final long serialVersionUID = -8631425323624976168L;
-	public UnknownErrorException()
-	{
-		super("An unknown error has occurred.");
-	}
-}
