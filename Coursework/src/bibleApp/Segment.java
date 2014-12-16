bdddf293ad874587191eef33bac3c859e22d5921
dmd1337 <dmd1337@hotmail.co.uk>
2014-12-16 12:03:53 +0000

    Segment stuff

--- Coursework/src/bibleApp/Segment.java
+++ Coursework/src/bibleApp/Segment.java
@@ -1,12 +1,9 @@
 package bibleApp;
-
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Stack;
 import exceptions.VerseOutOfRangeException;
-
 /**
- * A segment is either a chapter or a psalm
+ * A segment is either a chapter or a psalm.
  * @author Norfolk-in-Chance (Thomas Slate, Jack Taylor, Abigail Boyton)
  */
 public class Segment
@@ -17,29 +14,45 @@ public class Segment
 		this.verses = verses;
 	}
 	/**
-	 * @return a string with all the verses
+	 * @return A string with all the verses
 	 */
 	public String toString()
 	{
 		int i = 1;
 		StringBuilder result = new StringBuilder();
-		while (verses.get(i+"") != null)
+		while (verses.get(i + "") != null)
 		{
-			result.append(verses.get(i+""));
+			result.append(verses.get(i + ""));
 			i++;
 		}
 		return result.toString();
 	}
-	public String getRange(int start, int end)
+	/**
+	 * Retrieves a range of verses.
+	 * @param start The first verse to be retrieved
+	 * @param end The last verse to be retrieved
+	 * @return The range of verses between start and end, inclusive
+	 * @throws VerseOutOfRangeException
+	 */
+	public String getRange(int start, int end) throws VerseOutOfRangeException
 	{
 		StringBuilder result = new StringBuilder();
 		for (int i = start; i <= end; i++)
 		{
 			result.append(verses.get(i + ""));
 		}
+		if (result.toString() == null)
+		{
+			throw new VerseOutOfRangeException();
+		}
 		return result.toString();
 	}
-	
+	/**
+	 * Retrieves a single verse.
+	 * @param index The index of the verse to be retrieved
+	 * @return The contents of the verse
+	 * @throws VerseOutOfRangeException
+	 */
 	public String getVerse(int index) throws VerseOutOfRangeException
 	{
 		String temp = verses.get(index + "");
@@ -53,9 +66,9 @@ public class Segment
 	public Stack<String> find(String search, Stack<String> results)
 	{
 		int i = 1;
-		while (verses.get(i+"") != null)
+		while (verses.get(i + "") != null)
 		{
-			String current = verses.get(i+"");
+			String current = verses.get(i + "");
 			if (current.contains(search))
 			{
 				String location = "";
