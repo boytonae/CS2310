9f022f98943a98ba73b1e47a979291e84e2ca460
dmd1337 <dmd1337@hotmail.co.uk>
2014-12-16 19:19:02 +0000

    Fixed UnknownErrorException

--- Coursework/src/exceptions/UnknownErrorException.java
+++ Coursework/src/exceptions/UnknownErrorException.java
@@ -1,5 +1,9 @@
 package exceptions;
-public class UnknownErrorException
+public class UnknownErrorException extends Exception
 {
-
+	private static final long serialVersionUID = -8631425323624976168L;
+	public UnknownErrorException()
+	{
+		super("An unknown error has occurred.");
+	}
 }
